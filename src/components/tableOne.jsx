024cebc181166f49914f8b832185dbcc9e7c6de5
amt-juantuah <oseijuantuah@gmail.com>
2025-03-12 15:50:04 +0000

    vision cursor

--- src/components/tableOne.jsx
+++ src/components/tableOne.jsx
@@ -16,9 +16,9 @@ const TableOne = ({item}) => {
 
   return (
     <div className="w-full group rounded mb-10 border border-indigo-500 transition-all duration-500">
-        <div className="w-full center justify-between bg-indigo-500 group-hover:bg-transparent rounded p-4 flex flex-row justify-between items-center border-b border-indigo-500">
+        <div className="w-full center justify-between bg-indigo-500 group-hover:bg-transparent rounded p-4 flex flex-row justify-between items-center border-b border-indigo-500 cursor-pointer" onClick={setDisplayFunction}>
             <h2 className='text-white group-hover:text-indigo-500 text-xl transition-all duration-500'>{title}</h2>
-            <div className={`${display[0] === "hidden" && "rotate-180"} cursor-pointer text-white group-hover:text-indigo-500 text-xl transition-all duration-200`} onClick={setDisplayFunction}>&#9650;</div>
+            <div className={`${display[0] === "hidden" && "rotate-180"}  text-white group-hover:text-indigo-500 text-xl transition-all duration-200`}>&#9650;</div>
             {/* <div className={`${display[1]} cursor-pointer text-white group-hover:text-indigo-500 text-xl transition-all duration-500`} onClick={setDisplayFunction}>&#9660;</div> */}
         </div>
         <div className={`${display[0] === "hidden" ? "max-h-0" : "max-h-96" } transition-all duration-1000 overflow-hidden`}>
