a15c33a5e8a3e120c9b4f1ac3f32fe854aec15ce
Richard Osei <richardosei@richards-mbp.home>
2025-04-18 21:22:15 +0100

    landing

--- src/app/_components/Home/Banner.jsx
+++ src/app/_components/Home/Banner.jsx
@@ -2,6 +2,8 @@
 
 import React, {useState, useEffect} from 'react'
 import { motion } from "framer-motion";
+import { services } from '../JSON';
+import Link from 'next/link';
 
 const Banner = () => {
   const [scrollY, setScrollY] = useState(0);
@@ -19,7 +21,8 @@ const Banner = () => {
       <div className="bg-white w-full h-full">
         <div className="relative overflow-hidden ">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+          { services.map(  (item, index) => (
+            <div key={index} className="grid grid-cols-1 md:grid-cols-2 gap-12 mb-20 items-center">
               {/* Left Column - Text & CTA */}              
               <motion.div
                 initial={{ opacity: 0, x: 50 }}
@@ -27,15 +30,13 @@ const Banner = () => {
                 transition={{ duration: 0.8, delay: 0.2 }}
                 className="text-center md:text-left"
               >
-                <h4 className="text-4xl md:text-3xl font-bold text-gray-500 mb-6">
-                  Companionship
+                <h4 className="text-4xl md:text-3xl font-bold text-indigo-600 mb-6">
+                  { item.name}
                 </h4>
                 <p className="text-gray-500 text-lg mb-8">
-                Our dedicated companions spend quality time with you or your loved one, 
-                engaging in meaningful conversations, sharing hobbies, or simply enjoying 
-                each other’s company to alleviate loneliness and foster connection
+                { item.longDescription }
                 </p>
-                <div className="space-y-4">
+                <Link href={item.href} passHref className="space-y-4">
                   <motion.button
                     whileHover={{ scale: 1.05 }}
                     whileTap={{ scale: 0.95 }}
@@ -43,7 +44,7 @@ const Banner = () => {
                   >
                     Explore further
                   </motion.button>
-                </div>
+                </Link>
               </motion.div>
 
 
@@ -54,7 +55,29 @@ const Banner = () => {
                 transition={{ duration: 0.8 }}
                 className="relative grid grid-cols-2 gap-4"
               >
-                <img
+
+                {item.submenu.slice(0,4).map((itx, idx) => (
+                  <img key={itx.id}
+                    src={itx.image}
+                    alt={itx.id}
+                    className="mx-auto shadow-2xl transform hover:scale-105 transition-transform duration-300 w-[16.5625rem] h-[11.375rem] object-cover sm:w-full sm:aspect-[3/2]"
+                    loading="lazy"
+                  />
+                ))}
+
+              </motion.div>
+            </div>
+            ))}
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Banner
+
+
+{/* <img
                   src="/assets/images/care1.jpg"
                   alt="Carer"
                   className="mx-auto shadow-2xl transform hover:scale-105 transition-transform duration-300"
@@ -77,13 +100,4 @@ const Banner = () => {
                   alt="Carer"
                   className="mx-auto shadow-2xl transform hover:scale-105 transition-transform duration-300"
                   loading="lazy"
-                />
-              </motion.div>
-            </div>
-          </div>
-        </div>
-      </div>
-  )
-}
-
-export default Banner
+                /> */}
